151b2f64365f2532e6e559f1d7b9acdb1cd3c891
tico.liu <tico.liu@temu.com>
2024-04-04 14:10:34 +0800

    feat: restart a specified number of processes

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # block-tools
 **区块链相关脚本**
 ## commons
-- [检测进程关键词并重启](./commons/restart.sh)
+- [检测进程关键词并重启指定数量进程](./commons/restart.sh)
 ## IONet
 - [IONet自动重启](./ionet/monitor_and_restart.sh)
--- commons/restart.sh
+++ commons/restart.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
 # 要检测的关键词
-KEY_WORDS="keyword"
+KEY_WORDS="echo"
 
 # 启动进程的命令
-COMMAND_TO_START="command"
+COMMAND_TO_START="echo 1"
+
+# 重启数量
+PROCESS_COUNT=1
 
 # 无限循环检查进程
 while true; do
     # 使用ps和grep命令计算进程数量
     COUNT=$(ps -ef | grep -v grep | grep "$KEY_WORDS" | wc -l)
     if [ $COUNT -eq 0 ]; then
-        echo "$(date): $KEY_WORDS is not running. Restarting..."
-        $COMMAND_TO_START
+        echo "[$(date)][$KEY_WORDS]进程数量不足，正在尝试启动至少 $PROCESS_COUNT 个实例..."
+        # 这里是启动进程的循环，确保至少启动PROCESS_COUNT个进程
+        for ((i=0; i<$PROCESS_COUNT; i++)); do
+          $COMMAND_TO_START
+        done
     else
-        echo "$(date): $PROCESS_NAME is running."
+        echo "[$(date)][$KEY_WORDS]进程的数量满足要求，当前数量: $COUNT"
     fi
-    sleep 1  # 每5秒检查一次
+    sleep 1  # 每1秒检查一次
 done
